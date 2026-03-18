c1be4b9d1b65ccffb6c20b7b62f0406753b629e5
RupCharley <rupcharley@gmail.com>
2026-03-18 16:48:39 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'rupert-v15';
+const CACHE = 'rupert-v23';
 const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
